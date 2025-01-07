3d3efc987f5ab4cc775112d85ef7ae9e954791b4
victorazevedoexe <jvlazevedo03@gmail.com>
2025-01-06 21:16:39 -0300

    Update main.c

--- projeto_pilhas_algoritmos/Pilha-C--main/src/main.c
+++ projeto_pilhas_algoritmos/Pilha-C--main/src/main.c
@@ -24,8 +24,16 @@ static void repl() {
 
 int main() {
     printf("Bem-vindo ao interpretador de máquina baseada em pilha.\n");
-    printf("Comandos disponíveis: push <valor>, add, sub, mul, div, print, exit\n");
+    printf("Comandos disponíveis:\n");
+    printf("- push <valor>: Empilha um valor literal.\n");
+    printf("- push <nome_da_variavel>: Empilha o valor de uma variável.\n");
+    printf("- pop <nome_da_variavel>: Atualiza uma variável com o topo da pilha.\n");
+    printf("- add, sub, mul, div: Realiza operações matemáticas.\n");
+    printf("- print: Mostra o conteúdo atual da pilha.\n");
+    printf("- exit: Encerra o programa.\n");
+
     repl();
     return 0;
 }
+
 
